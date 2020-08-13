1171e3342f2a4b73deff715aea965c551d76599c
Will Gonzalez <will.colocho@gmail.com>
2020-08-13 05:24:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
      names.each do |name|
-       binding.pry
+       #binding.pry
+       if !new_array[name]
+         new_array[name]
+         binding.pry
       end
     end
    new_array
