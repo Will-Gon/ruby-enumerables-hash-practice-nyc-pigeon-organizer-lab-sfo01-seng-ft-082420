40cbd26323878b4ff54df7ae3e85f0af27815258
Will Gonzalez <will.colocho@gmail.com>
2020-08-13 04:46:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   binding.pry
-  data.each_with_object({}) |(key, value), new_array|
+  data.each_with_object({}) |key, value, new_array|
   new_array
 end
   binding.pry
