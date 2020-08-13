a72910e8a17336bd5fd4480ded3b4d5b9b7750cf
Will Gonzalez <will.colocho@gmail.com>
2020-08-13 05:40:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
        if !new_array[name][key]
          new_array[name][key] = []
       end
-     fi[name][key].push(inner_key.to_s)
+     final_array[name][key].push(inner_key.to_s)
     end
   end
 end
